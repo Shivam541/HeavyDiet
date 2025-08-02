eec41450f30a9c7cd06e7ebf471bccfde8b7e7d5
Shivam Garg <shivamgargsnp2017@gmail.com>
2025-08-03 02:58:06 +0530

    Initial commit of my Heavy Diet project

---
+++ src/main/java/live/HeavyDiet/HeavyDietApplication.java
@@ -0,0 +1,13 @@
+package live.HeavyDiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavyDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavyDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/HeavyDiet/HeavyDietApplicationTests.java
@@ -0,0 +1,13 @@
+package live.HeavyDiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavyDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
